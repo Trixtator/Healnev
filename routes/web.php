97cf5295ec596fd8d14bf987cfa30822303b9ea5
Miqdad-Dzaki <2311501005@unisayogya.ac.id>
2025-07-15 23:02:57 +0700

    ulangi lagii

--- routes/web.php
+++ routes/web.php
@@ -363,3 +363,6 @@ Route::get('/invoice/{id}/status', [InvoiceController::class, 'checkStatus'])->n
 
 // routes/web.php
 Auth::routes(['verify' => true]);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
