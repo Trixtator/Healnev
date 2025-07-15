a90dc5781a0055b4fb8c2860a1eb7221d76d4b3a
Miqdad-Dzaki <2311501005@unisayogya.ac.id>
2025-07-16 01:18:18 +0700

    bismillah

--- routes/web.php
+++ routes/web.php
@@ -361,8 +361,8 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/invoice/{id}/status', [InvoiceController::class, 'checkStatus'])->name('invoice.status');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
-    ->name('home')
-    ->middleware(['auth', 'verified']);
+// routes/web.php
+Auth::routes(['verify' => true]);
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
